a8f2608ac5777ead5b00373bd7b98f73e936e257
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-06-20 22:24:44 -0700

    working on buttons and carousel

--- components/details.tsx
+++ components/details.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import styles from '@/styles/Home.module.css'
+
 export default function Details({ props }) {
   const { product, price, size, details } = props;
+  const productName = product?.replace(/-/g, ' ').replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+  const sizes = ['XS', 'S', 'M', 'L', 'XL', '1X', '2X', '3X'];
+
+  const [activeButton, setActiveButton] = useState('M');
+
+  const handleClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
+
+
+  let i = 0;
+  const buttonSizes: JSX.Element[] = sizes.map((size) => {
+    i++
+    return (
+      <button className={styles.buttonSizes} key={i}>
+        {size}
+      </button>
+    )
+  })
   return (
-    <div>
-      <h1>{product}</h1>
-      <p>{price}</p>
-      <button>{size}</button>
+    <div className={styles.details}>
+      <h1>{productName}</h1>
+      <p>${price}</p>
+      <h3>Size</h3>
+      <div className=''>
+        {buttonSizes}
+      </div>
       <h3>Product Details</h3>
       <p>{details}</p>
     </div>
--- pages/products/[product].tsx
+++ pages/products/[product].tsx
@@ -47,7 +47,41 @@ export default function ProductPage({ data }) {
     details: 'details',
     ...data
   }
-
+  /*
+    var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
+    var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
+    var loop = $this.data('loop') ? $this.data('loop') : false;
+    var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 4;
+    var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 3;
+    var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 2.5;
+    var spaceBetween = $this.data('space-between') ? $this.data('space-between') : 20;
+  
+    var swiper = new Swiper('.swiper-slider-' + index, {
+      direction: 'horizontal',
+      loop: loop,
+      freeMode: freeMode,
+      spaceBetween: spaceBetween,
+      breakpoints: {
+        1920: {
+          slidesPerView: slidesDesktop
+        },
+        992: {
+          slidesPerView: slidesTablet
+        },
+        320: {
+          slidesPerView: slidesMobile
+        }
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        dragSize: dragSize
+      }
+  */
   return (
     <div className={styles.product}>
       {/* <section>
@@ -56,21 +90,37 @@ export default function ProductPage({ data }) {
         <p>Query Parameter: {variant}</p>
       </section> */}
       <Swiper
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={1.2}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }}
+        scrollbar={{
+          el: '.swiper-scrollbar',
+          draggable: true,
+          snapOnRelease: true,
+          dragSize: 'auto',
+        }}
+        freeMode={true}
+        centeredSlides={true}
         // zoom={true}
-        navigation // Add navigation arrows
-        pagination={{ clickable: true }} // Add pagination dots
+        // scrollbar={{ draggable: true }}
+        // navigation={true}
+        // pagination={{ clickable: true }} // Add pagination dots
         className={styles.swiper}
       >
-        <SwiperSlide>
+        <SwiperSlide className={styles.slide}>
           <img src='https://cdn.shopify.com/s/files/1/0237/3346/9261/products/file_fbe029d5-ce6f-4a4b-ad03-12de748de70c_1680x.jpg?v=1674955469' alt='image1' />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.slide}>
           <img src='https://cdn.shopify.com/s/files/1/0237/3346/9261/products/file_d92bb309-b1c6-44fc-b0f6-b7a33a90d693_1100x.jpg?v=1674955469' alt='image1' />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.slide}>
           <img src='https://cdn.shopify.com/s/files/1/0237/3346/9261/products/file_ead9228a-a716-445a-8905-34caea5b18fa_1100x.jpg?v=1674955469' alt='image1' />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.slide}>
           <img src='https://cdn.shopify.com/s/files/1/0237/3346/9261/products/file_86ebb7b3-f1ec-4650-a3db-1983b106db83_1100x.jpg?v=1674955469' alt='image1' />
         </SwiperSlide>
       </Swiper>
